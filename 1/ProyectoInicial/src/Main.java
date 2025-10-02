d9afd9ba45c00a1d37923dbc5cdfdeddbc694e4e
Milikituki <myriam1869@gmail.com>
2025-10-02 19:50:10 +0200

    Update Main.java

--- 1/ProyectoInicial/src/Main.java
+++ 1/ProyectoInicial/src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    // Los comentarios pueden ir dentro o fuera de los métodos
+    /*
+    Para párrafos de comentarios
+    y no solo en una linea
+     */
+    /* TODO me falta esto (para las cosas que te quedan por hacer,
+     asi lo puedes buscar en View, tool window y TODO */
+
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Esta es la segunda linea del programa en Java");
+
+    }
+
+    public void nombreMetodo (){
+
+    }
+
 }
